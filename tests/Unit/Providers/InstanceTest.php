bc56d954b04b1c3ac58cf57c14d2a6b7cb19704d
César Correia <cesardcorreia@gmail.com>
2026-01-15 21:30:49 +0000

    moved test to more appropriate section

--- tests/Unit/Providers/InstanceTest.php
+++ tests/Unit/Providers/InstanceTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use CsarCrr\InvoicingIntegration\Client;
+use CsarCrr\InvoicingIntegration\Contracts\IntegrationProvider\Client\CreateClient;
 use CsarCrr\InvoicingIntegration\Contracts\IntegrationProvider\Invoice\CreateInvoice;
 use CsarCrr\InvoicingIntegration\Enums\IntegrationProvider;
 use CsarCrr\InvoicingIntegration\Invoice;
@@ -10,4 +12,10 @@ it('returns an instance of CreateInvoice', function (IntegrationProvider $provid
     config()->set('invoicing-integration.provider', $provider->value);
 
     expect(Invoice::create())->toBeInstanceOf(CreateInvoice::class);
+})->with('providers');
+
+it('returns an instance of CreateClient', function (IntegrationProvider $provider) {
+    config()->set('invoicing-integration.provider', $provider->value);
+
+    expect(Client::create())->toBeInstanceOf(CreateClient::class);
 })->with('providers');
