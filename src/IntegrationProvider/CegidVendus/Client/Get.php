42010f8b7ed1cdb4455f89ddcfbbe7986d51f4a7
César Correia <cesardcorreia@gmail.com>
2026-01-19 22:49:31 +0000

    fix CI tests

--- src/IntegrationProvider/CegidVendus/Client/Get.php
+++ src/IntegrationProvider/CegidVendus/Client/Get.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace CsarCrr\InvoicingIntegration\IntegrationProvider\CegidVendus\Client;
 
 use CsarCrr\InvoicingIntegration\Contracts\IntegrationProvider\Client\GetClient;
