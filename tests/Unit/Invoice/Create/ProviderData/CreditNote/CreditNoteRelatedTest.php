2722a785af22acf86d013c91845d5e972aa036a2
César Correia <cesardcorreia@gmail.com>
2026-02-22 22:06:31 +0000

    fix credit note reason now using invoicedata

--- tests/Unit/Invoice/Create/ProviderData/CreditNote/CreditNoteRelatedTest.php
+++ tests/Unit/Invoice/Create/ProviderData/CreditNote/CreditNoteRelatedTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use CsarCrr\InvoicingIntegration\Data\InvoiceData;
 use CsarCrr\InvoicingIntegration\Data\ItemData;
 use CsarCrr\InvoicingIntegration\Enums\InvoiceType;
 use CsarCrr\InvoicingIntegration\Enums\Provider;
@@ -9,11 +10,10 @@ use CsarCrr\InvoicingIntegration\Exceptions\Invoice\Items\MissingRelatedDocument
 use CsarCrr\InvoicingIntegration\Facades\Invoice;
 
 it('fails when no reason is provided on the item', function (Provider $provider) {
-    $invoice = Invoice::create();
-    $item = ItemData::from(['reference' => 'reference-1']);
-
-    $invoice->type(InvoiceType::CreditNote);
-    $invoice->item($item);
-
+    $invoice = Invoice::create(InvoiceData::make([
+        'type' => InvoiceType::CreditNote,
+        'items' => [ItemData::from(['reference' => 'reference-1'])]
+    ]));
+    
     $invoice->getPayload();
 })->with('providers')->throws(MissingRelatedDocumentException::class);
