b876d17d72eec629c0ae091c45629390f0a5dd5c
César Correia <cesardcorreia@gmail.com>
2026-02-22 22:03:55 +0000

    fix: credit note reason tests now use invoicde data

--- tests/Unit/Invoice/Create/ProviderData/CreditNote/CreditNoteReasonTest.php
+++ tests/Unit/Invoice/Create/ProviderData/CreditNote/CreditNoteReasonTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use CsarCrr\InvoicingIntegration\Data\InvoiceData;
 use CsarCrr\InvoicingIntegration\Data\ItemData;
 use CsarCrr\InvoicingIntegration\Data\PaymentData;
 use CsarCrr\InvoicingIntegration\Data\RelatedDocumentReferenceData;
@@ -14,41 +15,39 @@ use CsarCrr\InvoicingIntegration\Facades\Invoice;
 it('transforms to provider payload with credit note reason', function (Provider $provider, string $fixtureName) {
     $data = fixtures()->request()->invoice()->invoiceTypes()->files($fixtureName);
 
-    $invoice = Invoice::create();
-    $item = ItemData::from([
-        'reference' => 'reference-1',
-        'relatedDocument' => RelatedDocumentReferenceData::from([
-            'documentId' => 'FT 01P2025/1',
-            'row' => 1,
-        ]),
-    ]);
-
-    $invoice->type(InvoiceType::CreditNote);
-    $invoice->item($item);
-    $invoice->payment(PaymentData::from([
-        'amount' => 1000,
-        'method' => PaymentMethod::CREDIT_CARD,
+    $invoice = Invoice::create(InvoiceData::make([
+        'type' => InvoiceType::CreditNote,
+        'items' => [ItemData::from([
+            'reference' => 'reference-1',
+            'relatedDocument' => RelatedDocumentReferenceData::from([
+                'documentId' => 'FT 01P2025/1',
+                'row' => 1,
+            ]),
+        ])],
+        'payments' => [PaymentData::from([
+            'amount' => 1000,
+            'method' => PaymentMethod::CREDIT_CARD,
+        ])],
+        'creditNoteReason' => 'Product damaged'
     ]));
-    $invoice->creditNoteReason('Product damaged');
 
     expect($invoice->getPayload())->toMatchArray($data);
 })->with('providers', ['credit_note']);
 
 it('fails when reason is not applied', function (Provider $provider) {
-    $invoice = Invoice::create();
-    $item = ItemData::from([
-        'reference' => 'reference-1',
-        'relatedDocument' => RelatedDocumentReferenceData::from([
-            'documentId' => 'FT 01P2025/1',
-            'row' => 1,
-        ]),
-    ]);
-
-    $invoice->type(InvoiceType::CreditNote);
-    $invoice->item($item);
-    $invoice->payment(PaymentData::from([
-        'amount' => 1000,
-        'method' => PaymentMethod::CREDIT_CARD,
+    $invoice = Invoice::create(InvoiceData::make([
+        'type' => InvoiceType::CreditNote,
+        'items' => [ItemData::from([
+            'reference' => 'reference-1',
+            'relatedDocument' => RelatedDocumentReferenceData::from([
+                'documentId' => 'FT 01P2025/1',
+                'row' => 1,
+            ]),
+        ])],
+        'payments' => [PaymentData::from([
+            'amount' => 1000,
+            'method' => PaymentMethod::CREDIT_CARD,
+        ])]
     ]));
 
     $invoice->getPayload();
@@ -57,21 +56,20 @@ it('fails when reason is not applied', function (Provider $provider) {
 it('ignores credit note reason when invoice type is not credit note', function (Provider $provider, string $fixtureName) {
     $data = fixtures()->request()->invoice()->invoiceTypes()->files($fixtureName);
 
-    $invoice = Invoice::create();
-    $item = ItemData::from([
-        'reference' => 'reference-1',
-        'relatedDocument' => RelatedDocumentReferenceData::from([
-            'documentId' => 'FT 01P2025/1',
-            'row' => 1,
-        ]),
-    ]);
-
-    $invoice->item($item);
-    $invoice->payment(PaymentData::from([
-        'amount' => 1000,
-        'method' => PaymentMethod::CREDIT_CARD,
+    $invoice = Invoice::create(InvoiceData::make([
+        'items' => [ItemData::from([
+            'reference' => 'reference-1',
+            'relatedDocument' => RelatedDocumentReferenceData::from([
+                'documentId' => 'FT 01P2025/1',
+                'row' => 1,
+            ]),
+        ])],
+        'payments' => [PaymentData::from([
+            'amount' => 1000,
+            'method' => PaymentMethod::CREDIT_CARD,
+        ])],
+        'creditNoteReason' => 'Product damaged'
     ]));
-    $invoice->creditNoteReason('Product damaged');
 
     expect($invoice->getPayload())->toMatchArray($data);
 })->with('providers', ['not_credit_note_reason_check']);
