8fa0ba2559a49917b1b5324d8910d7e08deee703
César Correia <cesardcorreia@gmail.com>
2026-03-04 22:15:04 +0000

    fix: removed unused

--- src/Contracts/IntegrationProvider/Item/ShouldCreateItem.php
+++ src/Contracts/IntegrationProvider/Item/ShouldCreateItem.php
@@ -2,8 +2,6 @@
 
 namespace CsarCrr\InvoicingIntegration\Contracts\IntegrationProvider\Item;
 
-use CsarCrr\InvoicingIntegration\Data\ClientData;
-use CsarCrr\InvoicingIntegration\Data\ItemData;
 use Illuminate\Support\Collection;
 
 interface ShouldCreateItem
