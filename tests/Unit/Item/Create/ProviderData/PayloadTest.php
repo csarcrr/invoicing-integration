f85fb5a19a9c8b9118c684f2b255a4fd7ee888a4
César Correia <cesardcorreia@gmail.com>
2026-03-04 22:16:52 +0000

    fix: improved for the current supported fields (wip)

--- tests/Unit/Item/Create/ProviderData/PayloadTest.php
+++ tests/Unit/Item/Create/ProviderData/PayloadTest.php
@@ -1,18 +1,19 @@
 <?php
 
+use CsarCrr\InvoicingIntegration\Data\CategoryData;
 use CsarCrr\InvoicingIntegration\Data\ItemData;
 use CsarCrr\InvoicingIntegration\Enums\Provider;
 use CsarCrr\InvoicingIntegration\Facades\Item;
-use Spatie\Ray\Payloads\Payload;
 
 it('transforms item data to provider payload', function (Provider $provider, string $fixture) {
     $payload = fixtures()->request()->item()->files($fixture);
 
     $data = Item::create(ItemData::make([
         'name' => 'Item Title',
-        'reference' => 'reference-1'
+        'reference' => 'reference-1',
+        'description' => 'Item Description',
+        'barcode' => 'barcode-1',
+        'category' => CategoryData::make(['id' => 1]),
     ]))->getPayload();
-
-    dd($data);
-    expect($data->getPayload())->toMatchArray($payload);
+    expect($data->toArray())->toMatchArray($payload);
 })->with('providers', ['create'])->only();
