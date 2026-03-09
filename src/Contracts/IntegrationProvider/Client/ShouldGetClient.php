1923a82ee7300bf505bd4e43e48071bc467b2118
César Correia <cesardcorreia@gmail.com>
2026-03-09 21:38:09 +0000

    fix: reuse interfaces to extend each others to prevent bloat implementations on each class

--- src/Contracts/IntegrationProvider/Client/ShouldGetClient.php
+++ src/Contracts/IntegrationProvider/Client/ShouldGetClient.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace CsarCrr\InvoicingIntegration\Contracts\IntegrationProvider\Client;
 
+use CsarCrr\InvoicingIntegration\Contracts\ShouldExecute;
 use CsarCrr\InvoicingIntegration\Data\ClientData;
 use CsarCrr\InvoicingIntegration\Data\InvoiceData;
 use Spatie\LaravelData\Data;
 
-interface ShouldGetClient
+interface ShouldGetClient extends ShouldExecute
 {
-    public function execute(): self;
 
     /**
      * @return ClientData
