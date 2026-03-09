aca39e5bf75f3638efab408c2173e4138294e027
César Correia <cesardcorreia@gmail.com>
2026-03-09 21:22:43 +0000

    fix: use dependency injection in order to be able to create more abstract methods in base

--- src/Contracts/IntegrationProvider/Invoice/ShouldCreateInvoice.php
+++ src/Contracts/IntegrationProvider/Invoice/ShouldCreateInvoice.php
@@ -6,12 +6,16 @@ namespace CsarCrr\InvoicingIntegration\Contracts\IntegrationProvider\Invoice;
 
 use CsarCrr\InvoicingIntegration\Data\InvoiceData;
 use Illuminate\Support\Collection;
+use Spatie\LaravelData\Data;
 
 interface ShouldCreateInvoice
 {
     public function execute(): self;
 
-    public function getInvoice(): InvoiceData;
+    /**
+     * @return InvoiceData
+     */
+    public function getInvoice(): Data;
 
     /**
      * @return Collection<string, mixed>
